f4c9c038d5cf32c7c74f5290aaf1569dabb41e88
Job Natanael Contreras Garzon <143237173+Job-Natanael-Contreras-Garzon@users.noreply.github.com>
2025-04-20 03:46:15 -0400

    place holder email

--- FrontEndEcommerce1p/src/app/features/auth/auth.module.ts
+++ FrontEndEcommerce1p/src/app/features/auth/auth.module.ts
@@ -1,26 +1,23 @@
-import { NgModule }            from '@angular/core';
-import { CommonModule }        from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';             // <— añadido
-import { SharedModule }        from '../../shared/shared.module'; // <— añadido
-import { AuthRoutingModule }   from './auth-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms'; // <— añadido
+import { SharedModule } from '../../shared/shared.module'; // <— añadido
+import { AuthRoutingModule } from './auth-routing.module';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
-import { LoginComponent }      from './login/login.component';
-import { RegisterComponent }   from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     MatIconModule,
     MatButtonModule,
     SharedModule,
-    AuthRoutingModule
-  ]
+    AuthRoutingModule,
+  ],
 })
 export class AuthModule {}
